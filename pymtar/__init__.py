214dea57947d2ca059d5b786b2c16d94a5bcf724
Colin ML Burnett <cmlburnett@gmail.com>
2021-08-13 20:36:43 -0500

    Start of args parsing

--- pymtar/__init__.py
+++ pymtar/__init__.py
@@ -4,4 +4,120 @@ pymtar -- python magnetic tape tar interface
 Stores file data in a sqlite database for rapid localization of files on a tape archive.
 """
 
+# Global libraries
+import datetime
+import hashlib
+import os
+import subprocess
+
+# Installed libraries
+import sqlite3
+
+# My installed libraries
+from sqlitehelper import SH, DBTable, DBCol, DBColROWID
+
+class db(SH):
+	"""DB schema"""
+
+	__schema__ = [
+		DBTable('tape',
+			DBColROWID(),
+			DBCol('manufacturer', 'text'), # Tape manufacturer
+			DBCol('model', 'text'), # Tape model number
+			DBCol('gen', 'text'), # Generation (eg, "LTO8RW")
+			DBCol('sn', 'text'), # Serial number on cartridge
+			DBCol('barcode', 'text'), # Standard LTO barcode, null if not used
+			DBCol('ptime', 'datetime') # Purchase date and time
+		),
+		# A tape "file" equivalent to a tar file
+		DBTable('tar',
+			DBColROWID(),
+			DBCol('id_tape', 'integer'), # Tape this tar belongs to
+			DBCol('num', 'integer'), # File number on the tape
+			DBCol('stime', 'datetime'), # Star write time
+			DBCol('etime', 'datetime'), # End write time
+			DBCol('access_cnt', 'integer'), # Read counter
+			DBCol('blk_offset', 'integer'), # Block offset on tape
+			DBCol('options', 'text'), # Options supplied to tar (eg, z, j)
+			DBCol('uname', 'text'), # uname -a value at time of write
+		),
+		# One row per file stored in a tar file
+		DBTable('tarfile',
+			DBColROWID(),
+			DBCol('id_tape', 'integer'), # Tape this file belongs to
+			DBCol('id_tar', 'integer'), # tar file this file belongs to
+			DBCol('fullpath', 'text'), # Full path
+			DBCol('fname', 'text'), # File name
+			DBCol('sz', 'integer'), # Size of file in bytes
+			DBCol('sha256', 'text') # sha256 hash
+		)
+	]
+
+	def open(self, rowfactory=None):
+		ex = os.path.exists(self.Filename)
+
+		super().open()
+
+		if not ex:
+			self.MakeDatabaseSchema()
+
+	def reopen(self):
+		super().reopen()
+
+	@staticmethod
+	def _now():
+		return datetime.datetime.utcnow()
+
+
+
+class mt:
+	def __init__(self, dev):
+		dev = os.path.abspath(dev)
+
+		parts = os.path.split(dev)
+		if parts[0] != '/dev':
+			raise Exception("Tape drive not a device: %s" % dev)
+		if not parts[1].startswith('n'):
+			raise Exception("Recommend aginst using anything but a non-rewinding tape drive: %s" % dev)
+
+		self._dev = dev
+
+		# Execute a command
+		self.status()
+
+	@staticmethod
+	def _run(*args, timeout=5):
+		r = subprocess.run(args, timeout=timeout, check=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+		return r.stdout.decode('ascii')
+
+	def status(self):
+		ret = self._run('mt', '-f', self._dev, 'status')
+		lines = ret.split('\n')
+		parts = lines[1].strip('.').split(',')
+		parts = [_.strip() for _ in parts]
+
+		fnum = int(parts[0].split('=')[-1])
+		blk = int(parts[1].split('=')[-1])
+		part = int(parts[2].split('=')[-1])
+
+		return (fnum, blk, part)
+
+	def rewind(self):
+		self._run('mt', '-f', self._dev, 'rewind')
+
+	def offline(self):
+		self._run('mt', '-f', self._dev, 'offline')
+
+	def bsf(self, cnt):
+		if cnt is not int:
+			raise Exception("bsf: cnt parameter must be an integer, got '%s'" % cnt)
+
+		self._run('mt', '-f', self._dev, 'bsf', cnt)
+
+	def fsf(self, cnt):
+		if cnt is not int:
+			raise Exception("fsf: cnt parameter must be an integer, got '%s'" % cnt)
+
+		self._run('mt', '-f', self._dev, 'bsf', cnt)
 
--- pymtar/__main__.py
+++ pymtar/__main__.py
@@ -1,11 +1,85 @@
 
+# Global libraries
+import argparse
+import json
 import sys
 
+# This library
 import pymtar
 
+class PrintHelpException(Exception): pass
+
+class actions:
+	@classmethod
+	def action_find(kls, args):
+		print('find', args)
+
+		if args.action[1] == 'tape.barcode':
+			kls.action_find_tape_barcode(args, args.action[2])
+
+		elif args.action[1] == 'tape.sn':
+			kls.action_find_tape_sn(args, args.action[2])
+
+		else:
+			raise PrintHelpException("Unrecognized find command: %s" % args.action[1])
+
+	@classmethod
+	def action_find_tape_barcode(kls, args, bcode):
+		print('find tape barcode', bcode)
+
+	@classmethod
+	def action_find_tape_sn(kls, args, sn):
+		print('find tape sn', sn)
+
+	@classmethod
+	def action_new(kls, args):
+		print('new', args)
+
+
+
 def main():
-	print("hi")
-	print(sys.argv)
+	p = argparse.ArgumentParser(add_help=False)
+	p.add_argument('-h', '--help', action='store_true', default=False, help='Show usage information')
+	p.add_argument('-f', '--file', default='/dev/nst0', help='Device file path')
+	p.add_argument('-j', '--json', default=False, action='store_true', help="Print responses, where appropriate, in JSON instead")
+	p.add_argument('action', nargs=argparse.REMAINDER, help='Action/command to execute')
+
+	acts = {}
+	acts['find'] = actions.action_find
+	acts['new'] = actions.action_new
+
+	action_help = """
+  Actions help:
+    find tape.barcode   Find tapes by barcode
+    find tape.sn        Find tapes by serial number
+    new file            Create a new file within a tar file
+    new tape            Create a new tape record
+    new tar             Create a new tar file
+"""
+
+	args = p.parse_args()
+	if args.help:
+		p.print_help()
+		print(action_help)
+		sys.exit(2)
+	print(args)
+
+	try:
+		if args.action[0] in acts:
+			acts[ args.action[0] ](args)
+	except PrintHelpException as e:
+		p.print_help()
+		print(action_help)
+
+		print("Error: %s" % str(e))
+		sys.exit(2)
+	return
+
+	mt = pymtar.mt(sys.argv[1])
+
+	r = mt.status()
+	print(r)
+	print(r)
 
 if __name__ == '__main__':
 	main()
